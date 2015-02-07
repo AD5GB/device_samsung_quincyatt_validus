232066725934a72a5706c491b41969c08fd06c30
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2015-02-07 12:00:56 -0500

    setup beanstalk builds
    Change-Id: I8f458426efb87efb2a3a56a57639d09b0f084d60

--- cm.mk
+++ cm.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/samsung/quincyatt/full_quincyatt.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/beanstalk/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common candy5 stuff.
+$(call inherit-product, vendor/beanstalk/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I717 TARGET_DEVICE=SGH-I717 BUILD_FINGERPRINT="samsung/SGH-I717/SGH-I717:4.1.2/JZO54K/I717UCMD3:user/release-keys" PRIVATE_BUILD_DESC="SGH-I717-user 4.1.2 JZO54K I717UCMD3 release-keys"
 
-PRODUCT_NAME := cm_quincyatt
+PRODUCT_NAME := bs_quincyatt
 PRODUCT_DEVICE := quincyatt
